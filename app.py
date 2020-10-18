f0ef97f8bc626a4ddef6008c0198fd9784ee91cf
jkarnam7 <k.jaswanth24@gmail.com>
2020-10-18 14:34:46 +0530

    Added Age To The Script

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
 #Calculate Persons Based On Year Of Birth#
-year = int(input("Please Enter The Year You Are Born:")
+name = str(input("Please Enter Your Good Name:"))
+year = int(input("Please Enter The Year You Are Born:"))
+print(year)
+currentage = 2020 - year
+print("Hello "+name )
+print("You are %s old ",currentage)
