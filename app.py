9a1e89cb398972c8620b76e974441d994c41d0be
jkarnam7 <k.jaswanth24@gmail.com>
2020-10-18 15:10:36 +0530

    Edited The Strings Of Age

--- app.py
+++ app.py
@@ -3,5 +3,4 @@ name = str(input("Please Enter Your Good Name:"))
 year = int(input("Please Enter The Year You Are Born:"))
 print(year)
 currentage = 2020 - year
-print("Hello "+name )
-print("You are %d old " % (currentage))
+print("Hello "+name + " You are years %d old " % (currentage))
