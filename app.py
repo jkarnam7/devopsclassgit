2c3dcf96dc166cd3d8331b894126cdac08fd0256
jkarnam7 <k.jaswanth24@gmail.com>
2020-10-18 17:52:24 +0530

    Months Added

--- app.py
+++ app.py
@@ -3,4 +3,6 @@ name = str(input("Please Enter Your Good Name:"))
 year = int(input("Please Enter The Year You Are Born:"))
 print(year)
 currentage = 2020 - year
+months = currentage*12
 print("Hello "+name + " You are years %d old " % (currentage))
+print("Hello "+name + " You are months %d old " % (months))
